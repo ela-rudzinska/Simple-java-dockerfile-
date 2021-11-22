cd249cdb3b0dc6a0f1f313d788d621e3d6b7b030
ela-rudzinska <71728327+ela-rudzinska@users.noreply.github.com>
2021-11-22 20:50:41 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+  public static void main(String[] args) {
+		int number = 1;
+		char alphabet = 'a';
+		
+
+		while (number <= 30 & alphabet <= 'z') {
+		    System.out.println("kod znaku " + alphabet + " " + number );
+		    alphabet++;
+		    number++;
+		}    
+  }
+}
